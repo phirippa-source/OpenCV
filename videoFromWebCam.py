64715aca2f191c134ad7e3d5cb460638f5ab7a9a
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-03-06 21:22:41 +0900

    Create videoFromWebCam.py

---
+++ videoFromWebCam.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if cap.isOpened():
+    while True:
+        ret, img = cap.read()
+        if ret:
+            cv2.imshow('camera', img)
+            if cv2.waitKey(1) != -1:
+                break;
+        else:
+            print('no frame')
+            break;
+else:
+    print(f"Can't open video{vidoe_file}!")
+
+cap.release()
+cv2.destroyAllWindows()
