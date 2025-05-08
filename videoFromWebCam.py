c296bc9013a6d8c4972191461931e3a60dc3ed04
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-05-08 17:55:41 +0900

    Update videoFromWebCam.py

--- videoFromWebCam.py
+++ videoFromWebCam.py
@@ -2,18 +2,21 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 if cap.isOpened():
     while True:
-        ret, img = cap.read()
+        ret, frame = cap.read()
         if ret:
-            cv2.imshow('camera', img)
+            cv2.imshow('camera', frame)
             if cv2.waitKey(1) != -1:
                 break;
         else:
             print('no frame')
             break;
 else:
-    print(f"Can't open video{vidoe_file}!")
+    print(f"Can't open a webcam!")
 
 cap.release()
 cv2.destroyAllWindows()
