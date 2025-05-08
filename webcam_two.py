63b98b142c073996fc4c98e6604f13fcf6aba9e5
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-05-08 18:05:45 +0900

    Create webcam_two.py

---
+++ webcam_two.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+if not cap.isOpened():
+    print('cap open failed!')
+    exit()
+else:
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow("webcam", frame)
+            if cv2.waitKey(1) != -1:
+                break
+        else:
+            print("Can't read cap")
+            break
+        
+    cap.release()
+    cv2.destroyAllwindows()
