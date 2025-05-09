4dcd3742043aa176206d2f987a8af280ef040997
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-05-09 21:31:05 +0900

    Update webcam_one.py

--- webcam_one.py
+++ webcam_one.py
@@ -1,10 +1,19 @@
-import cv2
+import cv2 as cv
+import sys
 
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)              # 카메라와 연결 시도
+if not cap.isOpened():
+  sys.exit('[fail] - Camera Connection')
 
-if cap.isOpened():
-  print("Webcam open ok~!")
-else:
-  print("Can't open a webcam!")
-
-cap.release()
+while True:
+  ret, frame = cap.read()
+  if not ret:
+    print('[fail] - no more frame')
+    break
+  cv.imshow('Webcam Display', frame)
+  key = cv.waitKey(1)                  # 1[ms] 동안 키보드 입력 기다림
+  if key == ord('q'):                  # 'q' 키가 눌리면 while 루프를 나감
+    break
+ 
+cap.release()                          #카메라와 연결 끊음
+cv.destroyAllWindows()
