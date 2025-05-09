05b86241b3d457309ac6472f50eb59b00bc14934
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-05-09 21:34:27 +0900

    Update webcam_two.py

--- webcam_two.py
+++ webcam_two.py
@@ -1,23 +1,24 @@
-import cv2
+import cv2 as cv
+import sys
 
-cap = cv2.VideoCapture(0)
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+cap = cv.VideoCapture(0)
 if not cap.isOpened():
-    print('cap open failed!')
-    exit()
-else:
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            cv2.imshow("webcam", frame)
-            if cv2.waitKey(1) != -1:
-                break
-        else:
-            print("Can't read cap")
-            break
-        
-    cap.release()
-    cv2.destroyAllWindows()
+  sys.exit('[fail] - Camera Connection')
+
+# cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+
+while True:
+  ret, frame = cap.read()
+  if not ret:
+    print('error')
+    break
+  cv.imshow('Webcam Display', frame)
+  
+  if cv.waitKey(1) == ord('q'):
+    break
+ 
+cap.release()
+cv.destroyAllWindows()
